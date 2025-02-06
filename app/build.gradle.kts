7b3e846dd9767cc6ed66eb47d7a1ba771de1aa7b
Andreas Vettefors <andreas@appcorn.se>
2025-02-06 10:17:06 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.compose.compiler)
+    id("maven-publish")
+}
+
+android {
+    namespace = "se.vettefors.randombox"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "se.vettefors.randombox"
+                artifactId = "randombox"
+                version = "0.1.0"
+
+                pom {
+                    name.set("$groupId:$artifactId")
+                    this.description.set("A random box")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            name.set("A Random box")
+                            email.set("randomvox@randombox.com")
+                            organization.set("Random box")
+                            organizationUrl.set("https://www.randombox.com/")
+                        }
+                    }
+
+                }
+
+                from(project.components["release"])
+            }
+        }
+    }
+
+}
+
+dependencies {
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
+}
---
+++ app/src/main/java/se/vettefors/randombox/SomeRandomClass.kt
@@ -0,0 +1,10 @@
+package se.vettefors.randombox
+
+/**
+ * Just a random class that is completely useless
+ */
+data class SomeRandomClass(
+    val title: String,
+    val message: String,
+    val somethingElse: Boolean
+)
